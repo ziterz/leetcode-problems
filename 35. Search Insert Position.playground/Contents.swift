82fce9bb1cccb48a0782dc8b8b1dd0e7ac9c60ce
Ziterz <ziady.mubaraq@gmail.com>
2023-12-27 14:45:35 +0700

    prepare for challenge number 35

---
+++ 35. Search Insert Position.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Created by Ziady Mubaraq on 27/12/2023
+
+import UIKit
+
+func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+  
+  return 0
+}
+
+searchInsert([1, 3, 5, 6], 5) // 2
+searchInsert([1, 3, 5, 6], 2) // 1
+searchInsert([1, 3, 5, 6], 7) // 4
