cbd33aaeb480f37a1b30f6c53d13366b8a0e19bc
Ziterz <ziady.mubaraq@gmail.com>
2023-12-27 15:09:37 +0700

    using binary search to solve challenge 35

--- 35. Search Insert Position.playground/Contents.swift
+++ 35. Search Insert Position.playground/Contents.swift
@@ -2,11 +2,36 @@
 
 import UIKit
 
+//func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+//  if target == 0 { return 0 }
+//  if nums.contains(target) {
+//      return nums.firstIndex(of: target) ?? 0
+//  } else {
+//      return (nums.firstIndex(of: target-1) ?? 0) + 1
+//  }
+//}
+
+// using binary search
 func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-  
-  return 0
+  var start: Int = 0
+  var end: Int = nums.count - 1
+
+  while start <= end {
+    let mid: Int = (start + end) / 2
+    if nums[mid] == target {
+      return mid
+    } else if nums[mid] > target {
+      end = mid - 1
+    } else {
+      start = mid + 1
+    }
+  }
+
+  return start
 }
 
-searchInsert([1, 3, 5, 6], 5) // 2
+searchInsert([2, 5], 1) // 0
 searchInsert([1, 3, 5, 6], 2) // 1
-searchInsert([1, 3, 5, 6], 7) // 4
+searchInsert([1, 3, 5, 6], 0) // 0
+searchInsert([1, 2, 3, 5, 6, 8, 10, 15], 7) // 5
+searchInsert([1, 2, 3, 5, 6, 8, 10, 15], 0) // 0
