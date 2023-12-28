82ad7e9484ae986dcadc1cd5752ae62fff4a107e
Ziterz <ziady.mubaraq@gmail.com>
2023-12-28 18:02:32 +0700

    solve length of last world with 4ms runtime

--- 58. Length of Last Word.playground/Contents.swift
+++ 58. Length of Last Word.playground/Contents.swift
@@ -1,9 +1,14 @@
 import UIKit
 
+//func lengthOfLastWord(_ s: String) -> Int {
+//  var dict: [String] = []
+//  for char in s.split(separator: " ").reversed() {
+//    dict.append(String(char))
+//  }
+//  return dict[0].count
+//}
+
 func lengthOfLastWord(_ s: String) -> Int {
-  var dict: [String] = []
-  for char in s.split(separator: " ").reversed() {
-    dict.append(String(char))
-  }
-  return dict[0].count
+  let word = s.split(separator: " ")
+  return word.last?.count ?? 0
 }
