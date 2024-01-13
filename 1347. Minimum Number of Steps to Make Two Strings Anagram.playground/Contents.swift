99332b9d58e4cc89d63ae748c4c2d04a667d0f20
Ziterz <ziady.mubaraq@gmail.com>
2024-01-13 14:31:55 +0700

    solve minimum number of steps to make twi strings anagram problems

--- 1347. Minimum Number of Steps to Make Two Strings Anagram.playground/Contents.swift
+++ 1347. Minimum Number of Steps to Make Two Strings Anagram.playground/Contents.swift
@@ -2,23 +2,41 @@
 
 import UIKit
 
+//class Solution {
+//  func minSteps(_ s: String, _ t: String) -> Int {
+//    let s = s.sorted()
+//    let t = t.sorted()
+//    print(s, t)
+//    var count: Int = 0
+//
+//    for i in 0..<s.count {
+//      if s[i] != t[i] {
+//        count += 1
+//      }
+//    }
+//    return count
+//  }
+//}
+
 class Solution {
   func minSteps(_ s: String, _ t: String) -> Int {
-    let s = s.sorted()
-    let t = t.sorted()
-    print(s, t)
-    var count: Int = 0
+    var dict = [Character:Int]()
     
-    for i in 0..<s.count {
-      if s[i] != t[i] {
-        count += 1
-      }
+    for i in s {
+      dict[i, default: 0] += 1
     }
-    return count
+    print(dict)
+    for i in t {
+      dict[i, default: 0] -= 1
+    }
+    print(dict)
+    let val =  dict.values.filter { $0 <= 0 }.reduce(0, +)
+    print(val)
+    return abs(val)
   }
 }
 
 let solution = Solution()
 solution.minSteps("bab", "aba") // 1
 solution.minSteps("leetcode", "practice") // 5
-solution.minSteps("anagram", "mangaar") // 0
+//solution.minSteps("anagram", "mangaar") // 0
